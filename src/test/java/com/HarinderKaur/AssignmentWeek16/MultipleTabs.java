c6417e00b00409063bde93fbc80ed48a47b2f4b8
HarinderKaur91 <kaur.harinder91@gmail.com>
2022-12-21 20:04:36 -0700

    Selenium Assignment

--- src/test/java/com/HarinderKaur/AssignmentWeek16/MultipleTabs.java
+++ src/test/java/com/HarinderKaur/AssignmentWeek16/MultipleTabs.java
@@ -1,6 +1,7 @@
 package com.HarinderKaur.AssignmentWeek16;
 
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -45,60 +46,48 @@ public class MultipleTabs {
 		WebElement fourthLink = wd.findElement(By.cssSelector("div.post-body.entry-content a:last-of-type"));
 
 		String parentWindowHandle = wd.getWindowHandle();
-		String child1Handle = null;
-		String child2Handle = null;
-		String child3Handle = null;
 		
-		//Navigating to first tab
+		// Navigating to first tab
 		firstLink.click();
-		Set<String> allWindowHandlesList = wd.getWindowHandles();
+		Set<String> allWindowHandlesList1 = wd.getWindowHandles();
 		System.out.println("Parent Window handle " + parentWindowHandle);
-		for (String handle : allWindowHandlesList) {
+		for (String handle : allWindowHandlesList1) {
 			if (!handle.equals(parentWindowHandle)) {
 				wd.switchTo().window(handle);
-				child1Handle = handle;
+				String child1Handle = handle;
+				System.out.println("Child1 handle is " + child1Handle);
 			}
 		}
-		System.out.println("Child1 handle is " + child1Handle);
 		sf.assertEquals(wd.getTitle(), "Google", "Title doesn't match");
 		wd.switchTo().window(parentWindowHandle);
-		
-		//Navigating to second tab
+
+		// Navigating to second tab
 		wdwait.until(ExpectedConditions
 				.elementToBeClickable(By.cssSelector("div.post-body.entry-content a:nth-of-type(2)")));
 		secondLink.click();
-		allWindowHandlesList = wd.getWindowHandles();
-		for (String handle : allWindowHandlesList) {
-			if (!(handle.equals(parentWindowHandle) || handle.equals(child1Handle))) {
-				wd.switchTo().window(handle);
-				child2Handle = handle;
-			}
-		}
-		System.out.println("Child2 handle is " + child2Handle);
+		Set<String> allWindowHandlesList2 = wd.getWindowHandles();
+		// Creating copy of set using copy constructor
+		Set<String> copyOfList2 = new HashSet<>(allWindowHandlesList2);
+		copyOfList2.removeAll(allWindowHandlesList1);
+		wd.switchTo().window(copyOfList2.iterator().next());
+		System.out.println("Child2 handle is " + copyOfList2);
 		sf.assertEquals(wd.getTitle(), "Facebook - log in or sign up", "Title doesn't match");
 		wd.switchTo().window(parentWindowHandle);
-	
+		
 		//Navigating to third tab
 		wdwait.until(ExpectedConditions
-				.elementToBeClickable(By.cssSelector("div.post-body.entry-content a:nth-of-type(3)")));
+				.elementToBeClickable(By.cssSelector("div.post-body.entry-content a:nth-of-type(2)")));
 		thirdLink.click();
-		allWindowHandlesList = wd.getWindowHandles();
-		for (String handle : allWindowHandlesList) {
-			if (!(handle.equals(parentWindowHandle) || handle.equals(child1Handle) || handle.equals(child2Handle))) {
-				wd.switchTo().window(handle);
-				child3Handle = handle;
-			}
-		}
-		System.out.println("Child3 handle is " + child3Handle);
-		sf.assertEquals(wd.getTitle(),
-				"Yahoo | Mail, Weather, Search, News, Finance, Sports, Shopping, Entertainment, Video",
-				"Title doesn't match");
+		Set<String> allWindowHandlesList3 = wd.getWindowHandles();
+		allWindowHandlesList3.removeAll(allWindowHandlesList2);
+		wd.switchTo().window(allWindowHandlesList3.iterator().next());
+		System.out.println("Child3 handle is " + allWindowHandlesList3);
+		sf.assertEquals(wd.getTitle(), "Yahoo | Mail, Weather, Search, News, Finance, Sports, Shopping, Entertainment, Video", "Title doesn't match");
 		wd.switchTo().window(parentWindowHandle);
-		
-	    //Navigating to fourth link on same window	
+
+		 //Navigating to fourth link on same window	
 		wdwait.until(
 				ExpectedConditions.elementToBeClickable(By.cssSelector("div.post-body.entry-content a:last-of-type")));
-
 		fourthLink.click();
 		sf.assertEquals(wd.getTitle(), "Facebook - log in or sign up", "Title doesn't match");
 		sf.assertAll();
